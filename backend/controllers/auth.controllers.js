adc05bcc46f1a54ffaa49c9c7694eddaa47ac12c
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-02-08 19:45:10 +0530

    Done with the sigIn controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,38 +1,70 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import genToken from "../utils/token.js";
+
 export const signUp = async (req, res) => {
-  try{
-    const {name, mobile, email, password, role} = await req.body;
-    const user = await User.findOne({email})
-    if(user){
-      return res.status(400).json({message:"User Already Exists"})
-    } 
-    if(password.length< 6){
-      return res.status(400).json({message:"password must be atleast 6 characters"})
+  try {
+    const { name, mobile, email, password, role } = await req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User Already Exists" });
     }
-    if(mobile.length < 10){
-      return res.status(400).json({message:"Mobile no must be atleast 10 digits"})
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be atleast 6 characters" });
+    }
+    if (mobile.length < 10) {
+      return res
+        .status(400)
+        .json({ message: "Mobile no must be atleast 10 digits" });
     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     user = await User.create({
       name,
       email,
       mobile,
       role,
-      password:hashedPassword
-    })
-    const token = await genToken(user._id)
-    res.cookie("token",token,{
-      secure:false,
-      sameSite:"strict",
-      maxAge:7*24*60*60*1000,
-      httpOnly:true
-    })
+      password: hashedPassword,
+    });
+    const token = await genToken(user._id);
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
 
     return res.status(201).json(user);
-  } catch (error){
-    return res.status(500).json(`sign up error ${error}`)
+  } catch (error) {
+    return res.status(500).json(`sign up error ${error}`);
   }
-}
+};
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = await req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect Password " });
+    }
+
+    const token = await genToken(user._id);
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(`sign In error ${error}`);
+  }
+};
+
