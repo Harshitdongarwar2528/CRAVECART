3eded97b89130527fc749e5415197ac479a9bdd8
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-02-16 19:46:37 +0530

    Done with the Forgot pass word and fixed the warnings in the pages

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -36,7 +36,7 @@ function SignIn() {
 
   return (
     <div className='min-h-screen w-full flex items-center justify-center p-4' style={{ backgroundColor: bgColor }}>
-      <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px] `} style={{
+      <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border `} style={{
         border: `1px solid ${borderColor}`
       }}>
         <h1 className={`text-3xl font-bold mb-2 `} style={{ color: primaryColor }}>CraveCart</h1>
@@ -57,7 +57,7 @@ function SignIn() {
           <div className='relative'>
             <input type={`${showPassword ? "text" : "password"}`} className='w-full border rounded-lg px-3 py-2 focus:outline-none pr-10' placeholder='Enter your password' style={{ border: `1px solid ${borderColor}` }} onChange={(e) => setPassword(e.target.value)} value={password} required />
 
-            <button className='absolute right-3 cursor-pointer top-[14px] text-gray-500' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaRegEye /> : <FaRegEyeSlash />}</button>
+            <button className='absolute right-3 cursor-pointer top-3.5 text-gray-500' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaRegEye /> : <FaRegEyeSlash />}</button>
           </div>
         </div>
                         <div className='text-right mb-4 cursor-pointer text-[#ff4d2d] font-medium' onClick={()=>navigate("/forgot-password")}>
